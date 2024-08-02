6ce525f13c9dfb351c9b5c5da13e045644d825fc
hirushaaaaaa <hirusha.fernando2002@gmail.com>
2024-08-02 15:39:00 +0530

    Update Vehicle.java

--- FuelWiseTripPlanner/Vehicle.java
+++ FuelWiseTripPlanner/Vehicle.java
@@ -1,22 +1,25 @@
-public class Vehicle{
-    private double fuelConsumptionRate;
-    private double fuelTankCapacity;
+public class Vehicle {
+    private double fuelConsumptionRate; 
+    private double fuelTankCapacity;    
 
-    public Vehicle(double fuelConsumptionRate, double fuelTankCapacity){
+    public Vehicle(double fuelConsumptionRate, double fuelTankCapacity) {
         this.fuelConsumptionRate = fuelConsumptionRate;
         this.fuelTankCapacity = fuelTankCapacity;
     }
 
-    public double calculateDistancePossible(double fuelAmount){
-        return fuelConsumptionRate * fuelAmount;
-    }
-
-    public double calculateFuelNeeded(double distance){
-        return distance / fuelConsumptionRate;
-    }
-
-    public double getFuelConsumptionRate(){
+    public double getFuelConsumptionRate() {
         return fuelConsumptionRate;
     }
 
+    public double getFuelTankCapacity() {
+        return fuelTankCapacity;
+    }
+
+    public double calculateDistancePossible(double fuelAmount) {
+        return fuelConsumptionRate * fuelAmount;
+    }
+
+    public double calculateFuelNeeded(double distance) {
+        return distance / fuelConsumptionRate;
+    }
 }
