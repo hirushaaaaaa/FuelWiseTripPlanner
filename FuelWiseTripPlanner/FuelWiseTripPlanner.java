78c35d3afa950d958c813fcd80ab1503f8dacb73
hirushaaaaaa <hirusha.fernando2002@gmail.com>
2024-08-02 15:01:25 +0530

    Update FuelWiseTripPlanner.java

---
+++ FuelWiseTripPlanner/FuelWiseTripPlanner.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class FuelWiseTripPlanner {
+     public static void main(Scanner[] arg) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter fuel consumption rate (km/l): ");
+        double fuelConsumptionRate = scanner.nextDouble();
+
+        System.out.println("Enter fuel tank capacity (liters): ");
+        double fuelTankCapacity = scanner.nextDouble();
+
+        Vehicle vehicle = new Vehicle(fuelConsumptionRate, fuelTankCapacity);
+
+        
+     }
+}
