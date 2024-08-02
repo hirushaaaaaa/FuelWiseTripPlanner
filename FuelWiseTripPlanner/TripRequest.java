f34d4c7eda0643bc9c129b590e9554877c41baeb
hirushaaaaaa <hirusha.fernando2002@gmail.com>
2024-08-02 14:27:01 +0530

    Create TripRequest.java

---
+++ FuelWiseTripPlanner/TripRequest.java
@@ -0,0 +1,23 @@
+public class TripRequest {
+    private double distance;
+    private double fuelPrice;
+    private double time;
+    
+    public TripRequest(double distance, double fuelPrice, double time){
+        this.distance = distance;
+        this.fuelPrice = fuelPrice;
+        this.time = time;
+    }
+
+    public double getDistance(){
+        return distance;
+    }
+
+    public double getFuelPrice(){
+        return fuelPrice;
+    }
+
+    public double getTime(){
+        return time;
+    }
+}
