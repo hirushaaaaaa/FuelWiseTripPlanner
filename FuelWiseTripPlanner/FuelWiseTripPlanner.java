4feac2d2401ef3ca8028427e1a572691b5f0beb8
hirushaaaaaa <hirusha.fernando2002@gmail.com>
2024-08-02 15:06:03 +0530

    Update FuelWiseTripPlanner.java

--- FuelWiseTripPlanner/FuelWiseTripPlanner.java
+++ FuelWiseTripPlanner/FuelWiseTripPlanner.java
@@ -13,5 +13,22 @@ public class FuelWiseTripPlanner {
         Vehicle vehicle = new Vehicle(fuelConsumptionRate, fuelTankCapacity);
 
         Queue<TripRequest> tripQueue = new LinkedList<>();
+
+        while(true){
+
+            System.out.println("\nChoose an option:");
+            System.out.println("1. Calculate Distance Possible");
+            System.out.println("2. Calculate Fuel Needed for Distance");
+            System.out.println("3. Calculate Fuel Consumption Rate");
+            System.out.println("4. Convert Miles to Kilometers");
+            System.out.println("5. Convert Kilometers to Miles");
+            System.out.println("6. Estimate Cost of Trip");
+            System.out.println("7. Validate Input");
+            System.out.println("8. Calculate Average Speed");
+            System.out.println("9. Find Cheapest Fuel Station");
+            System.out.println("10. Add Trip Request to Queue");
+            System.out.println("11. Process Trip Request Queue");
+            System.out.println("12. Exit");
+        }
      }
 }
