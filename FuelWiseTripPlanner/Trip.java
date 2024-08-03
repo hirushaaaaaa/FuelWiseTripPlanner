c7866ece5c76bcb07c25320830f3ed720b0ac1a3
sithijagimhan <sithijagimhan19@gmail.com>
2024-08-03 13:16:20 +0530

    Update Trip.java

--- FuelWiseTripPlanner/Trip.java
+++ FuelWiseTripPlanner/Trip.java
@@ -3,14 +3,15 @@ public class Trip {
     private double fuelPrice; 
     private double time; 
 
-    public Trip(double distance, double fuelPrice, double time) {
-        this.distance = distance;
-        this.fuelPrice = fuelPrice;
-        this.time = time;
+    public Trip(double d, double fp, double t) {
+        distance = d;
+        fuelPrice = fp;
+        time = t;
     }
 
     public double estimateCost(double fuelConsumptionRate) {
-        return (distance / fuelConsumptionRate) * fuelPrice;
+        double fuelNeeded = distance / fuelConsumptionRate;
+        return fuelNeeded * fuelPrice;
     }
 
     public double calculateAverageSpeed() {
